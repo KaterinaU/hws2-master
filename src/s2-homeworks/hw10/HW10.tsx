15e30970874b3cf2185f665a12d8dbf0dea067fa
Ekaterina <ushakova.esu@gmail.com>
2023-04-15 21:18:21 +0600

    hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -15,12 +15,19 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+
+    const dispatch = useDispatch();
+    const isLoading = useSelector<AppStoreType>(state => state.loading.isLoading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
+        const action = loadingAC(true);
+        dispatch(action);
 
-        // setTimeout
+           // setTimeout
+        setTimeout(()=>{
+            dispatch(loadingAC(false))
+        }, 1500)
     }
 
     return (
